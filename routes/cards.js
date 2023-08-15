70a974002671264813006dea44da146ea32e81c8
Andrey <aakondrash@yandex.ru>
2023-08-15 15:19:35 +0300

    2

--- routes/cards.js
+++ routes/cards.js
@@ -6,11 +6,31 @@ const {
   setCardLike,
   removeCardLike,
 } = require('../controllers/cards');
+const { celebrate, Joi } = require('celebrate');
+const { URL_REGEXP_PATTERN } = require('../utils/constants');
+
 
 router.get('/cards', getAllCards);
-router.post('/cards', createCard);
-router.delete('/cards/:_id', deleteCard);
-router.put('/cards/:_id/likes', setCardLike);
-router.delete('/cards/:_id/likes', removeCardLike);
+router.post('/cards', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(URL_REGEXP_PATTERN),
+  }),
+}), createCard);
+router.delete('/cards/:_id', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex().required(),
+  }),
+}), deleteCard);
+router.put('/cards/:_id/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex().required(),
+  }),
+}), setCardLike);
+router.delete('/cards/:_id/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex().required(),
+  }),
+}), removeCardLike);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,26 @@ const {
   setAvatar,
   setProfile
 } = require('../controllers/users');
+const { URL_REGEXP_PATTERN } = require('../utils/constants');
+const {celebrate, Joi} = require('celebrate');
 
 router.get('/users', getAllUsers);
 router.get('/users/me', getUserInfo);
-router.post('/users', createUser);
-router.get('/users/:userId', getUserInfo);
-router.patch('/users/me/avatar', setAvatar);
-router.patch('/users/me', setProfile);
+router.get('/users/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().length(24).hex().required(),
+  }),
+}), getUserInfo);
+router.patch('/users/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().pattern(URL_REGEXP_PATTERN),
+  }),
+}), setAvatar);
+router.patch('/users/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), setProfile);
 
 module.exports = router;
